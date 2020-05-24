7d6b82a2dcc974315ad186c7ed7d8f8db9719a7f
Your Name <you@example.com>
2020-05-24 16:09:42 +0800

    1

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Constants from "expo-constants";
 import * as Location from "expo-location";
 import { Icon } from "react-native-elements";
 import metroJson from "./json/metro.json"
-import axios from "axios";
+
 
 const UBIKE_URL =
   "https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview";
